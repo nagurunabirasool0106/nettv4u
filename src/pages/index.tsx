c5d81a2638929625c001e32bfe394bf7b4cfc76e
hariraju0106 <hariraju0106@gmail.com>
2023-09-17 14:11:37 +0530

    appbar2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,17 @@
 import { api } from '~/utils/api'
 import Image from 'next/image'
 import ResponsiveAppBar from './components/appbar'
+import ResponsiveAppBar2 from './components/appbar2'
+import Newsdropdown from './components/newsdropdown'
+import Photogallerydropdown from './components/photogallerydropdown'
+import Playdropdown from './components/playdropdown'
+import Reviewsdropdown from './components/reviewsdropdown'
+import Videosdropdown from './components/videosdropdown'
+import Articlesdropdown from './components/articlesdropdown'
+import Celebritydropdown from './components/celebritydropdown'
+import Tvserialsdropdown from './components/tvserialsdropdown'
+import Moredropdown from './components/moredropdowm'
+
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: 'from tRPC' })
@@ -13,7 +24,18 @@ export default function Home() {
   return (
     <>
       <ResponsiveAppBar />
+      <ResponsiveAppBar2 />
+      <Newsdropdown/>
+      <Photogallerydropdown/>
+      <Playdropdown/>
+      <Reviewsdropdown/>
+      <Videosdropdown/>
+      <Articlesdropdown/>
+      <Celebritydropdown/>
+      <Tvserialsdropdown/>
+      <Moredropdown/>
       {/* <p>next js is running on material ui</p> */}
     </>
+    
   )
 }
